6f6d9bad3c5eb22af4a602585eb50f09174d04e5
Thinggun <lmk2430@ajou.ac.kr>
2024-12-06 09:01:10 +0900

    [FE]: 초기 Head 및 아이콘 추가, css파일 삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import PokemonList from './pages/PokemonList';
-import PokemonDetail from './pages/PokemonDetail';
-import Auth from './pages/Auth';
-import Ranking from './pages/Ranking';
-import './App.css';
-import { AuthProvider } from './contexts/AuthContext';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import PokemonList from "./pages/PokemonList";
+import PokemonDetail from "./pages/PokemonDetail";
+import MyPokemon from "./pages/MyPokemon";
+import Gacha from "./pages/Gacha";
+import Auth from "./pages/Auth";
+import Ranking from "./pages/Ranking";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
-    return (
-        <AuthProvider>
-            <Router>
-                <div className="App">
-                    <Header />
-                    <Routes>
-                        <Route path="/auth" element={<Auth />} />
-                        <Route path="/" element={<PokemonList />} />
-                        <Route path="/pokemon/:id" element={<PokemonDetail />} />
-                        <Route path="/ranking" element={<Ranking />} />
-                    </Routes>
-                </div>
-            </Router>
-        </AuthProvider>
-    );
+  return (
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/" element={<PokemonList />} />
+            <Route path="/pokemon/:id" element={<PokemonDetail />} />
+            <Route path="/mypokemon" element={<MyPokemon />} />
+            <Route path="/gacha" element={<Gacha />} />
+            <Route path="/ranking" element={<Ranking />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;
